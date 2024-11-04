6e72ac56cc796b34b572f86180707c718c49f6ee
Purwanshi08 <purwanshi.bajaj300@gmail.com>
2024-11-05 00:28:27 +0530

    heading changed

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -32,25 +32,24 @@ export default function AboutPage() {
     // </div>
 
     <div className="about">
-    <h1 className='reveal'>ABOUT PAGE</h1>
-    <h2 className='reveal'>About Us</h2>
-    <h3 className='reveal'>Empowering Your Financial Journey with Data-Driven Insights</h3>
-    <p className='reveal'>At Stock Trekker, we believe that informed investing is powerful investing. Our platform combines real-time data, cutting-edge analysis, and intuitive tools to bring you a clear, actionable view of the stock market. Whether you're an experienced investor or just beginning your journey, our mission is to equip you with the insights needed to make confident, strategic decisions in a fast-moving market.</p>
-    <h2 className='reveal'>Our Mission</h2>
+    <h1 className='reveal'>About Us</h1>
+    <h3 className='reveal'>Empowering your Financial Journey with Data-Driven Insights</h3>
+    <p className='reveal'>At Stock Trekker, we believe that informed investing is powerful investing and we provide you that, comibining real-time data, cutting-edge tracking, and intuitive tools to bring you a clear, actionable view of the stock market. Whether you're an experienced investor or a beignner, our mission is to equip you with the insights needed to make confident, strategic decisions in a fast-moving market.</p>
+    <h1 className='reveal'>Our Mission</h1>
     <h3 className='reveal'>To Simplify Market Complexity and Drive Financial Success</h3>
     <p className='reveal'>In today's ever-changing financial landscape, timely and accurate information is key. We are dedicated to providing a straightforward, user-friendly experience that transforms complex data into meaningful insights, helping you spot opportunities, manage risks, and achieve your investment goals with confidence.</p>
-    <h2 className='reveal'>Why Choose Us?</h2>
+    <h1 className='reveal'>Why Choose Us?</h1>
     <ul className='reveal'>
       <li className='reveal'><h3>Real-Time Data</h3></li>
       <p className='reveal'>Stay up-to-date with the latest market movements and trends to make timely decisions.</p>
-      <li className='reveal'><h3>In-Depth Analysis Tools</h3></li>
-      <p className='reveal'>Our suite of tools allows you to dissect market data, assess stock performance, and understand the financial landscape like never before.</p>
+      <li className='reveal'><h3>In-Depth Tracking Tools</h3></li>
+      <p className='reveal'>Our suite of tools allows you to see market data, assess stock performance, and understand the financial landscape like never before.</p>
       <li className='reveal'><h3>Intuitive and User-Friendly Interface</h3></li>
       <p className='reveal'>Easy navigation and clear visuals let you dive deep into stock insights without getting lost in numbers.</p>
       <li className='reveal'><h3>Actionable Insights for Every Investor</h3></li>
-      <p className='reveal'>Whether short-term trading or long-term investing, our analysis adapts to your strategies, bringing value to investors of every level.</p>
+      <p className='reveal'>Whether mid cap companies or large cap enterprises, our tracking adapts to your strategies, bringing value to investors of every level.</p>
     </ul>
-    <h2 className='reveal'>Our Vision</h2>
+    <h1 className='reveal'>Our Vision</h1>
     <h3 className='reveal'>Creating a Community of Confident Investors</h3>
     <p className='reveal'>Our vision is to democratize access to advanced market insights, fostering a world where investors of all backgrounds can make sound financial choices. We strive to create a community where knowledge is shared, confidence is built, and success is achieved collectively.</p>
   </div>
