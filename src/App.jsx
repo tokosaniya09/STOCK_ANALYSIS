546261b9971776df2dd29be2bcab0f43efa0e3b3
Purwanshi08 <purwanshi.bajaj300@gmail.com>
2024-11-05 20:48:02 +0530

    imports changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import StockInfo from "./components/StockInfo"; 
-import Home from "./pages/HomePage";
 import Footer from "./components/Footer"; 
 import { Outlet } from "react-router-dom";
-import AboutPage from "./pages/AboutPage";
-import { ClerkProvider } from '@clerk/clerk-react';
 
 function App() {
   return (
